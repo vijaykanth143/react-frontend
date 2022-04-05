4cf1e2f99a5b5c48515f80606d6af41ba7672df1
Himasekhar Pothapola <pothapolahimasekhar@gmail.com>
2022-04-05 15:27:41 +0530

    some changes on success and failed registration

--- src/components/EmployeeRegFailed.jsx
+++ src/components/EmployeeRegFailed.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './EmployeeRegFailed.css';
+import {VscError} from 'react-icons/vsc'
 
 export default function EmployeeRegFailed() {
 	return (
-		<div className="employee-registration-failed-home">
-			<h1>Oops!</h1>
+		<div className="employee-registration-failed-home zoomIn">
+			<VscError className="failed-icon"/>
+			<h1>Oops something went wrong</h1>
 		</div>
 	);
 }
--- src/components/EmployeeRegSuccess.jsx
+++ src/components/EmployeeRegSuccess.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import {IoCheckmarkDoneCircleOutline} from 'react-icons/io5';
 import './EmployeeRegSucess.css';
 
 export default function EmployeeRegSuccess() {
 	return (
-		<div className="employee-registration-success-home">
+		<div className="employee-registration-success-home zoomIn ">
+			<IoCheckmarkDoneCircleOutline className="success-icon"/>
 			<h1>Employee Registered Successfully</h1>
+			
 		</div>
 	);
 }
